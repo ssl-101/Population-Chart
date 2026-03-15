feb00a43b261f0a99a1ca1ccdba2c319b4be4c26
ssl-101 <Inquiresiham@gmail.com>
2026-03-15 01:47:30 +0000

    Cookie ingredient calculator

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
-//This Program calculates energy drink consumption.
+//This program will display the ingredient ratio for a cookie recipe.
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
 
-//Given information
-  const double customers = 16500; // customers surveyed 
-  const double percentPurchase = 0.15; // purchased one or more  
-  const double percentCitrus = 0.58; // citrus flavor purchase 
+//base recipie
+  const double sugar_for_one = 1.5 / 48; // sugar amount
+  const double butter_for_one = 1.0 / 48; // butter amount  
+  const double flour_for_one = 2.75 / 48; // flour amount
+
+  double input_amount;
+
+  cout << "Enter the amount of cookies you are making: ";
+  cin >> input_amount; 
 
 //Calculations
-  const double oneorMore = customers * percentPurchase;
-  const double citrus = oneorMore * percentCitrus;
+  const double required_sugar = sugar_for_one * input_amount;
+  const double required_butter = butter_for_one * input_amount;
+  const double required_flour = flour_for_one * input_amount;
 
 //Conclusion
-  cout << "In the survey taken of 16,500 customers, \n" << oneorMore << " customers purchased one or more energy drinks." <<endl;
-  cout << "Out of the customers that purchased one or more energy drinks,\n" << citrus << " customers purchased citrus flavored energy drinks." <<endl;
+  cout << fixed << setprecision(2);
+  cout << "Sugar:  " << required_sugar << "  cups"<< endl;
+  cout << "Butter:  " << required_butter << "  cups" << endl;
+  cout << "Flour:  " << required_flour << "  cups"<< endl;
 
   return 0;
 }
