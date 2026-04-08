b5ee7250e4e687bc46744e46a869c222f477be74
ssl-101 <Inquiresiham@gmail.com>
2026-04-08 02:36:00 +0000

    geometry calculator program.

--- main.cpp
+++ main.cpp
@@ -1,45 +1,75 @@
-//This program calculates rate of a package
+//This program calculates the Area.
 #include <iostream>
 #include <cmath>
-#include <iomanip>
+#include <numbers>
 using namespace std;
 
 int main()
 {
 
 //Variables
-double RATE, DISTANCE, WEIGHT;
+int length, width, radius, height, base;
+double pi = 3.141592653589793;
+int choice; 
   
-  cout << "Enter package weight in kg: "<< endl;
-  cin >> WEIGHT;
+  cout << "Choose an option (1-4): "<< endl;
+ 
+  
+  cout << "1. Calculate the Area of a Circle."<<endl;
+  cout << "2. Calculate the Area of a Rectangle."<<endl;
+  cout << "3. Calculate the Area of a Triangle."<<endl;
+  cout << "4. Exit."<<endl;
+  
+  
+  cin >> choice; 
 
- if (WEIGHT <= 0 || WEIGHT > 20 )
- { 
-   cout <<"invalid weight entry, must be between 1 and 20 kg."<< endl;
+ if (choice <= 0 || choice > 4 )
+ {
+   cout << "Invalid entry. Choose a number between 1-4."<<endl;
    return 0;
  }
-  cout << "Enter distance in miles:"<< endl;
-  cin >> DISTANCE;
- 
-   if (DISTANCE < 10 || DISTANCE >3000)
+ switch(choice)
    {
-     cout<< "Invalid distance entry, must be between 10 and 3000 miles"<< endl;
-     return 0;
+     case 1:
+       
+       cout <<"Enter the radius of the circle:"<< endl;
+       cin>> radius;
+      if(radius <0)
+      { 
+         cout << "Invalid entry. Radius can not be less than zero."<< endl;
+         return 0;
+      }
+       cout<< "Area:"<< pi * radius * radius <<endl;
+      break;
+     case 2: 
+       cout << "Enter the length of the rectangle:"<<endl;
+       cout << "Enter the Width of the Rectangle:"<<endl;
+       cin >> length;
+       cin >> width;
+      if (length < 0 || width <0)
+      {
+       cout<< "Invalid entry. Length and width can not be less than zero." << endl;
+        return 0;
+      }
+       cout<<"Area:"<< length * width << endl;
+      break;
+     case 3:
+       cout << "Enter the base of the triangle:"<<endl;
+       cout<< " Enter the height of the triangle:"<<endl;
+       cin >> base; 
+       cin >> height;
+      if (base <0 || height <0)
+      {
+        cout<< "Invalid Entry. Base and height can not be less than zero." << endl;
+        return 0;
+      }
+       cout <<"Area:"<< 0.5 * base * height << endl;
+      break;
+     case 4:
+       cout<< "You have exited the program."<< endl;
+     
    }
-  
-//Cost per weight
-   if (WEIGHT <=2) RATE = 1.10;
-    else if (WEIGHT > 2 && WEIGHT <= 6) RATE = 2.20;
-    else if (WEIGHT > 6 && WEIGHT <= 10) RATE = 3.70;
-   else RATE = 4.80;
-
-//Distance calculations
-
- double TOTAL = ceil(DISTANCE / 500) * RATE ;
-  cout << fixed << setprecision(2)<< showpoint;
-  cout << "Total cost: $"<< TOTAL << endl;
-
-
-
+     
+      
   return 0;
 }
